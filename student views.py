c41430a920202cfabf5adb6c96dc429382d28ffe
Ashwin Ramesh <ashwinramesh568@gmail.com>
2026-09-08 13:15:02 +0530

    initial commit db connection established

---
+++ student views.py
@@ -0,0 +1,15 @@
+from mysql import connector
+
+class EmployeeManager:
+    def get_connection(self):
+        try:
+            self.connection = connector.connect(
+                host="localhost",
+                user="root",
+                password="Ashwin@123",
+                database="company_db"
+            )
+            return self.connection
+        except Exception as e:
+            
+            return None
